d37f6677098f3d0dcfd6ccc7107ff255d65179da
Joao Sacramento <sacramentofilipe@gmail.com>
2024-04-10 13:11:35 +0200

    fix for google cloud deployment #2

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -1,124 +1,124 @@
-import React, { useEffect, useState } from "react";
-import { api, handleError } from "helpers/api";
-import { Spinner } from "components/ui/Spinner";
-import { Button } from "components/ui/Button";
-import { useNavigate } from "react-router-dom";
-import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
-import "styles/views/Game.scss";
-import { User } from "types";
+// import React, { useEffect, useState } from "react";
+// import { api, handleError } from "helpers/api";
+// import { Spinner } from "components/ui/Spinner";
+// import { Button } from "components/ui/Button";
+// import { useNavigate } from "react-router-dom";
+// import BaseContainer from "components/ui/BaseContainer";
+// import PropTypes from "prop-types";
+// import "styles/views/Game.scss";
+// import { User } from "types";
 
-const Player = ({ user, onClick }: { user: User; onClick: () => void }) => (
-  <div className="player container" onClick={onClick}>
-    <span style={{ marginRight: 8 }}>
-      {user.status === "OFFLINE" ? "🔴" : "🟢"}
-    </span>
-    <div className="player username">{user.username}</div>
-    <div className="player name">{user.name}</div>
-    <div className="player id">id: {user.id}</div>
-  </div>
-);
+// const Player = ({ user, onClick }: { user: User; onClick: () => void }) => (
+//   <div className="player container" onClick={onClick}>
+//     <span style={{ marginRight: 8 }}>
+//       {user.status === "OFFLINE" ? "🔴" : "🟢"}
+//     </span>
+//     <div className="player username">{user.username}</div>
+//     <div className="player name">{user.name}</div>
+//     <div className="player id">id: {user.id}</div>
+//   </div>
+// );
 
-Player.propTypes = {
-  user: PropTypes.object,
-  onClick: PropTypes.func,
-};
+// Player.propTypes = {
+//   user: PropTypes.object,
+//   onClick: PropTypes.func,
+// };
 
-const Game = () => {
-  // use react-router-dom's hook to access navigation, more info: https://reactrouter.com/en/main/hooks/use-navigate
-  const navigate = useNavigate();
+// const Game = () => {
+//   // use react-router-dom's hook to access navigation, more info: https://reactrouter.com/en/main/hooks/use-navigate
+//   const navigate = useNavigate();
 
-  // define a state variable (using the state hook).
-  // if this variable changes, the component will re-render, but the variable will
-  // keep its value throughout render cycles.
-  // a component can have as many state variables as you like.
-  // more information can be found under https://react.dev/learn/state-a-components-memory and https://react.dev/reference/react/useState
-  const [users, setUsers] = useState<User[]>(null);
+//   // define a state variable (using the state hook).
+//   // if this variable changes, the component will re-render, but the variable will
+//   // keep its value throughout render cycles.
+//   // a component can have as many state variables as you like.
+//   // more information can be found under https://react.dev/learn/state-a-components-memory and https://react.dev/reference/react/useState
+//   const [users, setUsers] = useState<User[]>(null);
 
-  const logout = async () => {
-    try {
-      const currentUserId = localStorage.getItem("userId");
+//   const logout = async () => {
+//     try {
+//       const currentUserId = localStorage.getItem("userId");
 
-      if (currentUserId) {
-        const response = await api.post("/users/logout", { id: currentUserId });
-      }
+//       if (currentUserId) {
+//         const response = await api.post("/users/logout", { id: currentUserId });
+//       }
 
-      localStorage.removeItem("userId");
-      navigate("/login");
-    } catch (error) {
-      alert(`Something went wrong during the logout: \n${handleError(error)}`);
-    }
-  };
+//       localStorage.removeItem("userId");
+//       navigate("/login");
+//     } catch (error) {
+//       alert(`Something went wrong during the logout: \n${handleError(error)}`);
+//     }
+//   };
 
-  // the effect hook can be used to react to change in your component.
-  // in this case, the effect hook is only run once, the first time the component is mounted
-  // this can be achieved by leaving the second argument an empty array.
-  // for more information on the effect hook, please see https://react.dev/reference/react/useEffect
-  useEffect(() => {
-    // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-    async function fetchData() {
-      try {
-        const response = await api.get("/users");
+//   // the effect hook can be used to react to change in your component.
+//   // in this case, the effect hook is only run once, the first time the component is mounted
+//   // this can be achieved by leaving the second argument an empty array.
+//   // for more information on the effect hook, please see https://react.dev/reference/react/useEffect
+//   useEffect(() => {
+//     // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+//     async function fetchData() {
+//       try {
+//         const response = await api.get("/users");
 
-        // delays continuous execution of an async operation for 1 second.
-        // This is just a fake async call, so that the spinner can be displayed
-        // feel free to remove it :)
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+//         // delays continuous execution of an async operation for 1 second.
+//         // This is just a fake async call, so that the spinner can be displayed
+//         // feel free to remove it :)
+//         await new Promise((resolve) => setTimeout(resolve, 1000));
 
-        // Get the returned users and update the state.
-        setUsers(response.data);
+//         // Get the returned users and update the state.
+//         setUsers(response.data);
 
-        // This is just some data for you to see what is available.
-        // Feel free to remove it.
-        console.log("request to:", response.request.responseURL);
-        console.log("status code:", response.status);
-        console.log("status text:", response.statusText);
-        console.log("requested data:", response.data);
+//         // This is just some data for you to see what is available.
+//         // Feel free to remove it.
+//         console.log("request to:", response.request.responseURL);
+//         console.log("status code:", response.status);
+//         console.log("status text:", response.statusText);
+//         console.log("requested data:", response.data);
 
-        // See here to get more data.
-        console.log(response);
-      } catch (error) {
-        console.error(
-          `Something went wrong while fetching the users: \n${handleError(
-            error
-          )}`
-        );
-        console.error("Details:", error);
-        alert(
-          "Something went wrong while fetching the users! See the console for details."
-        );
-      }
-    }
+//         // See here to get more data.
+//         console.log(response);
+//       } catch (error) {
+//         console.error(
+//           `Something went wrong while fetching the users: \n${handleError(
+//             error
+//           )}`
+//         );
+//         console.error("Details:", error);
+//         alert(
+//           "Something went wrong while fetching the users! See the console for details."
+//         );
+//       }
+//     }
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  return (
-    <BaseContainer className="game container">
-      <h2>Happy Coding!</h2>
-      <p className="game paragraph">Get all users from secure endpoint:</p>
+//   return (
+//     <BaseContainer className="game container">
+//       <h2>Happy Coding!</h2>
+//       <p className="game paragraph">Get all users from secure endpoint:</p>
 
-      {!users ? (
-        <Spinner />
-      ) : (
-        <div className="game">
-          <ul className="game user-list">
-            {(users || []).map((user: User) => (
-              <li key={user.id} style={{ cursor: "pointer" }}>
-                <Player
-                  user={user}
-                  onClick={() => navigate(`/users/${user.id}`)} // This navigates to /game/id
-                />
-              </li>
-            ))}
-          </ul>
-          <Button width="100%" onClick={() => logout()}>
-            Logout
-          </Button>
-        </div>
-      )}
-    </BaseContainer>
-  );
-};
+//       {!users ? (
+//         <Spinner />
+//       ) : (
+//         <div className="game">
+//           <ul className="game user-list">
+//             {(users || []).map((user: User) => (
+//               <li key={user.id} style={{ cursor: "pointer" }}>
+//                 <Player
+//                   user={user}
+//                   onClick={() => navigate(`/users/${user.id}`)} // This navigates to /game/id
+//                 />
+//               </li>
+//             ))}
+//           </ul>
+//           <Button width="100%" onClick={() => logout()}>
+//             Logout
+//           </Button>
+//         </div>
+//       )}
+//     </BaseContainer>
+//   );
+// };
 
-export default Game;
+// export default Game;
