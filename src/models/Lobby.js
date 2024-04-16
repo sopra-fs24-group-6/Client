e7f80f9f4f2bc4a399c0e4e5f3675fc6c5b6ddca
Joao Sacramento <sacramentofilipe@gmail.com>
2024-04-16 19:57:23 +0200

    Revamp of lobby and regitration views to match the app theme. Issues #89 & #90. Followed by some minor bug fixes

--- src/models/Lobby.js
+++ src/models/Lobby.js
@@ -1,18 +1,18 @@
 class Lobby {
-    constructor(data = {}) {
-        this.id = null
-        this.lobbyAdmin = null
-        this.name = null;
-        this.password = null;
-        this.players = null;
-        this.playerLimit = null;
-        this.PlayerCount = null;
-        this.themes = null;
-        this.rountTimer = null;
-        this.clueTimer = null;
-        this.discussionTimer = null;
-        Object.assign(this, data);
-    }
+  constructor(data = {}) {
+    this.id = null;
+    this.lobbyAdmin = null;
+    this.name = null;
+    this.password = null;
+    this.players = null;
+    this.playerLimit = null;
+    this.PlayerCount = null;
+    this.themes = null;
+    this.rountTimer = null;
+    this.clueTimer = null;
+    this.discussionTimer = null;
+    Object.assign(this, data);
+  }
 }
 
 export default Lobby;
