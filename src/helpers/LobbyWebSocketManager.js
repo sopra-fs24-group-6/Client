4fa512cd316e580dffdc4dca5a06436524578c00
Elekidding <nico.pluess@uzh.ch>
2024-04-24 20:56:17 +0200

    small changes to startgame logic in lobby

--- src/helpers/LobbyWebSocketManager.js
+++ src/helpers/LobbyWebSocketManager.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Client } from "@stomp/stompjs";
 import { getBrokerURL } from "helpers/getBrokerURL";
 
-export const useLobbyWebSocket = (lobbyId, lobbyCallback, playerCallback) => {
+export const useLobbyWebSocket = (lobbyId, startGameCallback, lobbyCallback, playerCallback) => {
   const [client, setClient] = useState(null);
   const [connected, setConnected] = useState(false);
   const [subscribed, setSubscribed] = useState(false); // New state variable
@@ -25,6 +25,16 @@ export const useLobbyWebSocket = (lobbyId, lobbyCallback, playerCallback) => {
     });
 
     const subscribeToChannels = (client) => {
+      // subscribe game events
+      // message has eventType<String>, like "startGame", "startRound".
+        client.subscribe(`/topic/${lobbyId}/gameEvents`, (message) => {
+          const event = JSON.parse(message.body);
+          const newLog = `(Event notification: ${event.eventType})`;
+          setGameLog((prevGameLog) => [...prevGameLog, newLog]);
+          if(event.eventType==="startGame") {
+            startGameCallback();
+          }
+        });
         client.subscribe(`/lobbies/${lobbyId}/lobby_info`, (message) => {
             const response = JSON.parse(message.body);
             console.log(response);
