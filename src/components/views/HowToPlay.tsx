c2e0137bd7203d1980eeb22003791b1bac9f3962
Elekidding <nico.pluess@uzh.ch>
2024-05-23 10:49:40 +0200

    small corrections to how to play

--- src/components/views/HowToPlay.tsx
+++ src/components/views/HowToPlay.tsx
@@ -24,7 +24,11 @@ const HowToPlay = () => {
         </div>
         <div className="Extension">
           <div className="center-container">
-            <NESContainerW title="" className="center style scrollable" scrollable={true}>
+            <NESContainerW
+              title=""
+              className="center style scrollable"
+              scrollable={true}
+            >
               <h2>Find a Lobby</h2>
               <p style={{ marginBottom: "80px" }}>
                 You can either{" "}
@@ -51,24 +55,34 @@ const HowToPlay = () => {
               </p>
               <h2>Clue Phase</h2>
               <p>
-                At the beginning of each round, one player is randomly assigned the
-                role of the{" "}
+                At the beginning of each round, one player is randomly assigned
+                the role of the{" "}
                 <span style={{ fontWeight: "bold", color: "red" }}>Wolf</span>.
                 Everyone else is{" "}
-                <span style={{ fontWeight: "bold", color: "green" }}>Villager</span>.
+                <span style={{ fontWeight: "bold", color: "green" }}>
+                  Villagers
+                </span>
+                .
               </p>
-              <p>The villagers all see the clue word, the wolf does not.</p>
+              <p>The villagers all receive the clue word, the wolf does not.</p>
               <p>
-                As <span style={{ fontWeight: "bold", color: "green" }}>villagers</span>
+                As{" "}
+                <span style={{ fontWeight: "bold", color: "green" }}>
+                  villagers
+                </span>
                 , type something that describes the clue word but isn&apos;t too
                 obvious so that the
-                <span style={{ fontWeight: "bold", color: "red" }}> wolf</span>{" "}
+                <span style={{ fontWeight: "bold", color: "red" }}>
+                  {" "}
+                  wolf
+                </span>{" "}
                 could figure it out.
               </p>
               <p style={{ marginBottom: "80px" }}>
-                As <span style={{ fontWeight: "bold", color: "red" }}>Wolf</span>,
-                try to figure out what the clue word might be and blend in by giving
-                fitting clues.
+                As{" "}
+                <span style={{ fontWeight: "bold", color: "red" }}>Wolf</span>,
+                try to figure out what the clue word might be and blend in by
+                giving fitting clues.
               </p>
               <h2>Discussion Phase</h2>
               <p>
@@ -76,18 +90,18 @@ const HowToPlay = () => {
                 weird clue, who took too long to submit their clue, etc.
               </p>
               <p style={{ marginBottom: "80px" }}>
-                Try to come to a consensus on who the wolf was.
+                Try to come to a consensus on who the wolf is.
               </p>
               <h2>Voting Phase</h2>
               <p style={{ marginBottom: "80px" }}>
                 Vote for the player that you think was the wolf this round.
               </p>
               <h2>Results</h2>
-              <p>The player with the most votes is exiled from the herd.</p>
+              <p>The player with the most votes is exiled from the town.</p>
               <p>
                 If it was the{" "}
-                <span style={{ fontWeight: "bold", color: "red" }}>wolf</span>, all
-                the
+                <span style={{ fontWeight: "bold", color: "red" }}>wolf</span>,
+                all the
                 <span style={{ fontWeight: "bold", color: "green" }}>
                   {" "}
                   villagers
@@ -96,9 +110,14 @@ const HowToPlay = () => {
               </p>
               <p style={{ marginBottom: "80px" }}>
                 If it was a{" "}
-                <span style={{ fontWeight: "bold", color: "green" }}>villager</span>,
-                only the
-                <span style={{ fontWeight: "bold", color: "red" }}> wolf</span>{" "}
+                <span style={{ fontWeight: "bold", color: "green" }}>
+                  villager
+                </span>
+                or there was a tie, only the
+                <span style={{ fontWeight: "bold", color: "red" }}>
+                  {" "}
+                  wolf
+                </span>{" "}
                 wins!
               </p>
             </NESContainerW>
